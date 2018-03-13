d6e13392e111825337d8191f935db09de970cb57
ottovbismarck <fritzj96@gmail.com>
2018-03-13 16:31:59 -0700

    Almost done with the MenuPlanner Logic.

--- js/menuplanner.js
+++ js/menuplanner.js
@@ -1,51 +1,45 @@
-// var para = document.createElement("p");
-// var Breakfast2 = document.createTextNode(amount + unit + type + calories);
-// para.appendChild(Breakfast2);
-// var element = document.getElementById("Breakfast2");
-// element.appendChild(para);
-class MenuItem {
-  constructor(id, amount, unit, food, calories) {
-    this.id = id
-    this.amount = amount
-    this.unit = unit
-    this.food = food
-    this.calories = calories
-  }
-  printitem() {
-    document.getElementById(this.id).innerHTML = "• " + this.amount +" " + this.unit +" "+ this.food +" "+ this.calories + " Calories"
-  }
-}
+// Actual code begins here.
+//Sets the variables that will be used to represent the divs to be shown/hidden
+const showLow  = document.getElementsByClassName("showlow");
+const showMed  = document.getElementsByClassName("showmed");
+const showHigh = document.getElementsByClassName("showhigh");
 
-const lowcalorie = () => {
-  let egg = new MenuItem("brk1row1", 1, "Whole","Egg",72)
-  let eggwhites = new MenuItem("brk1row2", .75, "Cup", "Egg Whites", 95)
-  let rawoats = new MenuItem("brk1row3", .5, "Cup", "Raw Oats", 150)
-  let freshberries = new MenuItem("brk1row4", .5, "Cup", "Fresh Berries", 40)
-  egg.printitem()
-  eggwhites.printitem()
-  rawoats.printitem()
-  freshberries.printitem()
+function lowSetting() {
+  //adds the "Display None" style to the divs I don't want shown.
+  for (i = 0; i < showMed.length; i++) {
+    showMed[i].classList.add("displaynone");
+  }
+  for (i = 0; i < showHigh.length; i++) {
+    showHigh[i].classList.add("displaynone");
+  }
+  //removes the "Display None" style from the correct divs.
+  for (i = 0; i < showLow.length; i++) {
+    showLow[i].classList.remove("displaynone");
+  }
 }
-const mediumcalorie = () => {
-  let egg = new MenuItem("brk2row1", 1, "Whole","Egg",72)
-  let eggwhites = new MenuItem("brk2row2", 1, "Cup", "Egg Whites", 126)
-  let rawoats = new MenuItem("brk2row3", 1, "Cup", "Raw Oats", 300)
-  let freshberries = new MenuItem("brk2row4", 1, "Cup", "Fresh Berries", 80)
-  egg.printitem()
-  eggwhites.printitem()
-  rawoats.printitem()
-  freshberries.printitem()
+function medSetting() {
+  //adds the "Display None" style to the divs I don't want shown.
+  for (i = 0; i < showLow.length; i++) {
+    showLow[i].classList.add("displaynone");
+  }
+  for (i = 0; i < showHigh.length; i++) {
+    showHigh[i].classList.add("displaynone");
+  }
+  //removes the "Display None" style from the correct divs.
+  for (i = 0; i < showMed.length; i++) {
+    showMed[i].classList.remove("displaynone");
+  }
 }
-const highcalorie = () => {
-  let egg = new MenuItem("brk3row1", 2, "Whole","Egg", 1)
-  let eggwhites = new MenuItem("brk3row2", 1.5, "Cup", "Egg Whites", 190)
-  let rawoats = new MenuItem("brk3row3", 1.5, "Cup", "Raw Oats", 450)
-  let freshberries = new MenuItem("brk3row4", 1.5, "Cup", "Fresh Berries", 120)
-  egg.printitem()
-  eggwhites.printitem()
-  rawoats.printitem()
-  freshberries.printitem()
+function highSetting() {
+  //adds the "Display None" style to the divs I don't want shown.
+  for (i = 0; i < showLow.length; i++) {
+    showLow[i].classList.add("displaynone");
+  }
+  for (i = 0; i < showMed.length; i++) {
+    showMed[i].classList.add("displaynone");
+  }
+  //removes the "Display None" style from the correct divs.
+  for (i = 0; i < showHigh.length; i++) {
+    showHigh[i].classList.remove("displaynone");
+  }
 }
-lowcalorie()
-mediumcalorie()
-highcalorie()
